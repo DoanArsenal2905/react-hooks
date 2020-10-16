8dd2a99491f25b4758ef41574510d30b79ef36cb
Doan Arsenal <doanarsenal1998@outlook.com>
2020-10-16 14:39:59 +0700

    color-box

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import ColorBox from './components/ColorBox';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Doan Arsenal</h1>
+      <ColorBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './ColorBox.scss';
+
+ColorBox.propTypes = {
+  
+};
+
+const getRandomColor = () => {
+  const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue'];
+  const randomIndex = Math.trunc(Math.random() * 5);
+  return COLOR_LIST[randomIndex];
+};
+
+function ColorBox() {
+  const [color, setColor] = useState(() => {
+    const initColor = localStorage.getItem('box-color') || 'deeppink';
+    console.log(initColor)
+    return initColor
+  }); 
+
+  const handleBoxClick = () => {
+    const newColor = getRandomColor();
+    setColor(newColor);
+    localStorage.setItem('box-color', newColor);
+  };
+
+  return (
+    <div
+      className="color-box"
+      style={{ background: color }}
+      onClick={handleBoxClick}
+    >
+    </div>
+  );
+}
+
+export default ColorBox;
